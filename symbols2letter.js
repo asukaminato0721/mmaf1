504b04a275f62e90c3379ac7c761df528877d25b
asukaminato <asukaminato@nyan.eu.org>
2023-05-24 01:37:02 +0900

    typo

--- symbols2letter.js
+++ symbols2letter.js
@@ -25,7 +25,7 @@ module.exports = {
 			'*=': 'TimesBy',
 			'/=': 'DivideBy',
 			'..': 'Repeated',
-			'...': 'ReplacedNull',
+			'...': 'RepeatedNull',
 			'->': 'Rule',
 			'|->': 'Function',
 			':>': 'RuleDelayed',
